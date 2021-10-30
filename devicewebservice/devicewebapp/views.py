9ab182021ac48d1995e6151967c3c98f0f44e788
jit.heng.c@gmail.com <jit.heng.c@gmail.com>
2021-10-30 08:18:03 +0800

    updated files 30/10/2021

--- devicewebservice/devicewebapp/views.py
+++ devicewebservice/devicewebapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 import pymongo
-
+import json
 from datetime import datetime as dt
 
 #for post request with query
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
@@ -30,9 +30,7 @@ def viewdevices(request):
         print(devconns)
 
     devdata = {'viewdevicesdata': viewdevices, 'viewdeviceconndata':viewdeviceconns}
-    return render(request,'devicewebapp/viewdevices_lab3_part3.html',context=devdata)
-    #return render(request,'devicewebapp/viewdevices_part3.html',context=devdata)
-    #return render(request,'devicewebapp/viewdevices.html',context=devdata)
+    return render(request,'devicewebapp/viewdevices.html',context=devdata)
 
 def devices(request,param1):
     dev_name = param1
@@ -65,18 +63,46 @@ def devices(request,param1):
 @csrf_exempt
 def postview(request):
     if request.method == 'POST':
+        #print("RequestPost Data\n{}".format(request.POST))
+        #print("RequestPost Data-Body\n{}".format(request.body))
+        #print("RequestPost Data-Content\n{}".format(request.content_type))
+        #print("RequestPost ContentParams\n{}".format(request.content_params))
+        #if request.content_type == 'application/json':
+        #    inputdata = request.body.decode('utf-8')
+        #    print("Json string: {}".format(inputdata))
+        #    jsoninput = dict(inputdata)
+        #    print("json received input:{}".format(json.loads(inputdata)))
         usrname = request.POST.get('username')
         passwd = request.POST.get('password')
-        print("username:{} and password:{}".format(usrname,passwd))
+        print("HTTP-POST: username:{} and password:{}".format(usrname,passwd))
+        return HttpResponse("POST Request data {} with {} sent- successful".format(usrname,passwd))
 
-        return HttpResponse("POST Request {} exist with {} - successful".format(usrname,passwd))
     elif request.method == 'GET':
-        if request.GET.get('q','nothing'):
-            qstring=request.GET.get('q',nothing)
-            print("query string:{}".format(qstring))
-            return HttpResponse("Query Done")
+        if request.GET.get('username',''):
+            #print("?username - RequestPost Data\n{}".format(request.GET))
+            #print("RequestPost Data-Body\n{}".format(request.body))
+            #print("RequestPost Data-Content\n{}".format(request.content_type))
+            #print("RequestPost Data-Content\n{}".format(request.META))
+            #print("RequestPost ContentParams\n{}".format(request.content_params))
+            usrname = request.GET.get('username','')
+            access_token=request.GET.get('token','')
+            if access_token != "":
+                print("username:{} | token:{} - HTTP-GET successful".format(usrname,access_token))
+                response_data = {"username": usrname, "token received": access_token}
+            else:
+                print("username:{} - HTTP-GET successful".format(usrname))
+                response_data = {"username": usrname, "time queried": dt.now().strftime("%d-%m-%Y %H:%M:%S")}
+            return JsonResponse(response_data)
+            #return HttpResponse("Request {} exist with {} - HTTP-GET successful".format(usrname,passwd))
         else:
-            usrname = request.GET.get('username')
-            passwd = request.GET.get('password')
-            print("username:{} and password:{} - GET successful".format(usrname,passwd))
-            return HttpResponse("GET Request {} exist with {} - successful".format(usrname,passwd))
+            #print("?not username - RequestPost Data\n{}".format(request.GET))
+            #print("RequestPost Data-Body\n{}".format(request.body))
+            #print("RequestPost Data-Content\n{}".format(request.content_type))
+            #print("RequestPost ContentParams\n{}".format(request.content_params))
+            qstring=request.GET.get('search','')
+            print("HTTP-GET: query string:{}".format(qstring))
+            return HttpResponse("HTTP-GET: Query Done - {}".format(qstring))
+
+    else:
+        print("Error - Request is Invalid {}".format(request))
+        return HttpResponse("Request {} with {} - HTTP-GET error in data".format(usrname,passwd))
